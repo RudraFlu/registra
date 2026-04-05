be25d603fc699f4674cdae5022e369fe228cc503
RudraFlu <rudrakumarsingh07@gmail.com>
2026-04-05 19:23:37 +0530

    started working on addExpense page added a segmented button to select between expense type

--- registra/lib/main.dart
+++ registra/lib/main.dart
@@ -13,6 +13,9 @@ Future<void> main() async {
   runApp(const MyApp());
 }
 
+TextStyle stly = GoogleFonts.poppins(
+  textStyle: TextStyle(color: Color(0xFF355872), fontSize: 16),
+);
 final supabase = Supabase.instance.client;
 
 class MyApp extends StatefulWidget {
@@ -62,17 +65,25 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Color(0xFFF7F8F0),
         surfaceTintColor: Colors.transparent,
         elevation: 5,
-        shape: RoundedRectangleBorder(
-          side: BorderSide(style: BorderStyle.solid),
+        shape: LinearBorder.bottom(
+          side: BorderSide(
+            style: BorderStyle.solid,
+            width: 1,
+            color: Color(0xFF355782),
+          ),
         ),
         actions: [
           IconButton(onPressed: () {}, icon: Icon(Icons.search_rounded)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.group_add_outlined)),
           SizedBox(width: 10),
         ],
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => expensePage()),
+          );
+        },
         backgroundColor: Color(0xFF355872),
         icon: Icon(Icons.add, color: Color(0xFFF7F8F0)),
         label: Text(
@@ -84,8 +95,9 @@ class _HomePageState extends State<HomePage> {
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
-        selectedItemColor: Color(0xFF355872),
-        unselectedItemColor: Color(0xFF7AAACE),
+        selectedItemColor: Color(0xFFF7F8F0),
+        unselectedItemColor: Color(0xFF9CD5FF),
+        backgroundColor: Color(0xFF355872),
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: 'Home'),
           BottomNavigationBarItem(icon: Icon(Icons.group), label: 'Groups'),
@@ -94,13 +106,16 @@ class _HomePageState extends State<HomePage> {
             icon: Icon(Icons.analytics_outlined),
             label: 'Insight',
           ),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Account'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.manage_accounts),
+            label: 'Account',
+          ),
         ],
       ),
       body: Center(
         child: Column(
           children: [
-            Divider(),
+            //Divider(),
             FutureBuilder(
               future: supabase
                   .from('expense_history')
@@ -108,50 +123,81 @@ class _HomePageState extends State<HomePage> {
                   .order('created_at', ascending: false),
               builder: (context, snapshot) {
                 if (!snapshot.hasData)
-                  return CircularProgressIndicator(color: Color(0xFF355872));
+                  return CircularProgressIndicator(color: Color(0xFFF7F8F0));
                 final data = snapshot.data as List;
                 return Expanded(
                   child: ListView.separated(
                     itemCount: data.length,
                     itemBuilder: (context, index) {
                       final item = data[index];
-                       Color col;
-                       if(item['source']=="spent"){
-                          col = Color.fromARGB(255, 190, 0, 0);
-                        }
-                        else{
-                          col = Color.fromARGB(255, 17, 146, 0);
-                        }
-                        DateTime date = DateTime.parse(item['created_at']);
-                        String formatted = "${date.day}/${date.month}/${date.year}";
-                      return ListTile(
-                        tileColor: Color(0xFFF7F8F0),
-                        leading: Icon(Icons.person),
-                        title: Text(
-                          item['note'],
-                          style: GoogleFonts.poppins(
-                            textStyle: TextStyle(
-                              color: Color(0xFF355872),
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold
+                      Color col;
+                      if (item['source'] == "spent") {
+                        col = Color.fromARGB(255, 190, 0, 0);
+                      } else {
+                        col = Color.fromARGB(255, 17, 146, 0);
+                      }
+                      IconData icon;
+                      if (item['category'] == "food") {
+                        icon = Icons.local_restaurant;
+                      } else if (item['category'] == "transport") {
+                        icon = Icons.local_taxi;
+                      } else if (item['category'] == "shopping") {
+                        icon = Icons.shopping_bag_outlined;
+                      } else if (item['category'] == "entertainment") {
+                        icon = Icons.movie_creation_outlined;
+                      } else if (item['category'] == "bills") {
+                        icon = Icons.receipt_long_outlined;
+                      } else if (item['category'] == "health") {
+                        icon = Icons.medical_services_outlined;
+                      } else {
+                        icon = Icons.category;
+                      }
+                      DateTime date = DateTime.parse(item['created_at']);
+                      String formatted =
+                          "${date.day}/${date.month}/${date.year}";
+                      return TweenAnimationBuilder(
+                        tween: Tween<double>(begin: 0, end: 1),
+                        duration: Duration(milliseconds: 1000 + (index * 100)),
+                        curve: Curves.easeOut,
+                        builder: (context, value, child) {
+                          return Opacity(
+                            opacity: value,
+                            child: Transform.translate(
+                              offset: Offset(0, 20 * (1 - value)),
+                              child: child,
+                            ),
+                          );
+                        },
+                        child: ListTile(
+                          tileColor: Color(0xFFF7F8F0),
+                          leading: Icon(
+                            icon,
+                            size: 30,
+                            color: Color(0xFF355872),
+                          ),
+                          title: Text(
+                            item['note'],
+                            style: GoogleFonts.poppins(
+                              textStyle: TextStyle(
+                                color: Color(0xFF355872),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
                           ),
-                        ),
-                        subtitle: Text(
-                          formatted,
-                          style: GoogleFonts.poppins(
-                            textStyle: TextStyle(
-                              color: Color(0xFF355872),
-                              fontSize: 12,
+                          subtitle: Text(
+                            formatted,
+                            style: GoogleFonts.poppins(
+                              textStyle: TextStyle(
+                                color: Color(0xFF355872),
+                                fontSize: 12,
+                              ),
                             ),
                           ),
-                        ),
-                        trailing: Text(
-                          "₹${item['amount']}",
-                          style: GoogleFonts.poppins(
-                            textStyle: TextStyle(
-                              color: col,
-                              fontSize: 16,
+                          trailing: Text(
+                            "₹${item['amount']}",
+                            style: GoogleFonts.poppins(
+                              textStyle: TextStyle(color: col, fontSize: 16),
                             ),
                           ),
                         ),
@@ -163,6 +209,86 @@ class _HomePageState extends State<HomePage> {
                   ),
                 );
               },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class expensePage extends StatefulWidget {
+  expensePage({super.key});
+
+  @override
+  State<expensePage> createState() => _expensePageState();
+}
+
+class _expensePageState extends State<expensePage> {
+  Set<String> _selection = {"Personal"};
+  void updateSelected(Set<String> newSelect) {
+    setState(() {
+      _selection = newSelect;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF7F8F0),
+      appBar: AppBar(
+        toolbarHeight: 70,
+        shape: LinearBorder.bottom(
+          side: BorderSide(
+            style: BorderStyle.solid,
+            width: 1,
+            color: Color(0xFF355782),
+          ),
+        ),
+        backgroundColor: Color(0xFFF7F8F0),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back),
+        ),
+        title: Text("Add expense", style: stly),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.check),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 10),
+            SegmentedButton(
+              showSelectedIcon: false,
+              segments: [
+                ButtonSegment<String>(
+                  value: "Personal",
+                  label: Text("Personal"),
+                ),
+                ButtonSegment<String>(value: "Group", label: Text("Group")),
+                ButtonSegment<String>(value: "Friend", label: Text("Friend")),
+              ],
+              style: SegmentedButton.styleFrom(
+                textStyle: GoogleFonts.poppins(
+                  textStyle: TextStyle(color: Color(0xFF355872), fontSize: 16),
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                ),
+                backgroundColor: Colors.white,
+                selectedForegroundColor: Colors.white,
+                selectedBackgroundColor: Color(0xFF355782),
+              ),
+              selected: _selection,
+              onSelectionChanged: updateSelected,
             ),
           ],
         ),
